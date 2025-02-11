d997c020799258a8546bd4b0ee577d555d14574b
Sagar Kothari <sag333ar@gmail.com>
2025-02-11 19:54:12 +0530

    first commit

---
+++ flutter_service_worker.js
@@ -0,0 +1,218 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"flutter_bootstrap.js": "b7e1a9338b5cfc5084f8d30c8eeb0574",
+"version.json": "cd917f58134fdfa83cbd6666371fddee",
+"index.html": "5bac35ff3d843e7f32cb07d7010fc04d",
+"/": "5bac35ff3d843e7f32cb07d7010fc04d",
+"main.dart.js": "f192e09ed88b3eca0a175d7bf3e7b6b5",
+"functions.js": "dabee99442c8d78892ce25245cf001ef",
+"flutter.js": "4b2350e14c6650ba82871f60906437ea",
+"favicon.png": "36520b0907a5e188d2ce484bca4c023d",
+"blurt-ecc.js": "bf7197196cf6b37db1016e530c12196f",
+"icons/Icon-192.png": "fc80fbbc6c0bbc6efc41fa049bec7ffd",
+"icons/Icon-maskable-192.png": "fc80fbbc6c0bbc6efc41fa049bec7ffd",
+"icons/Icon-maskable-512.png": "b97f758507ac097e31603629380d90e3",
+"icons/Icon-512.png": "b97f758507ac097e31603629380d90e3",
+"manifest.json": "315e72ea320a3a555cc3182c72ac2240",
+"assets/AssetManifest.json": "9916860d77bc73dd3193f4d673c170c4",
+"assets/NOTICES": "ad9b894b9ede20f1d40566ac0706c71a",
+"assets/FontManifest.json": "f96929f2c90632cdf69ae10a3f79de45",
+"assets/AssetManifest.bin.json": "a75e69874fc66d022d0e3abe0932101e",
+"assets/packages/giphy_picker/assets/PoweredBy_200px-Black_HorizText.png": "439da1ed3ca70fb090eb98698485c21e",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b93248a553f9e8bc17f1065929d5934b",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"assets/AssetManifest.bin": "be04b3f74e80bda12a64f9a08a7cd1df",
+"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
+"assets/assets/images/logo.png": "ea9556fc2e2dc0de572a0c6ebe53bfb0",
+"assets/assets/fonts/poppins/Poppins-ExtraLight.ttf": "a0f2eadb0002101ea5da28381ce16cde",
+"assets/assets/fonts/poppins/Poppins-Light.ttf": "1bb10326b972c92be6c59c048a4327e4",
+"assets/assets/fonts/poppins/Poppins-Medium.ttf": "614a91afc751f09d049231f828801c20",
+"assets/assets/fonts/poppins/Poppins-ExtraBold.ttf": "4252dae8574e434c7bb0127b8ffd06df",
+"assets/assets/fonts/poppins/Poppins-Regular.ttf": "cd6b896a19b4babd1a2fa07498e9fc47",
+"assets/assets/fonts/poppins/Poppins-Bold.ttf": "124cd57d8f41f6db22a724f882dca3f4",
+"assets/assets/fonts/poppins/Poppins-Black.ttf": "9a3a3c0803219892727ed9d85ad5caea",
+"assets/assets/fonts/poppins/Poppins-SemiBold.ttf": "0fc985df77c6b59d37e79b97ed7fb744",
+"assets/assets/fonts/poppins/Poppins-Italic.ttf": "07502c4fe46025097dd8b1e331182ee0",
+"canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
+"canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
+"canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
+"canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
+"canvaskit/chromium/canvaskit.js.symbols": "e115ddcfad5f5b98a90e389433606502",
+"canvaskit/chromium/canvaskit.js": "b7ba6d908089f706772b2007c37e6da4",
+"canvaskit/chromium/canvaskit.wasm": "ea5ab288728f7200f398f60089048b48",
+"canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
+"canvaskit/canvaskit.wasm": "e7602c687313cfac5f495c5eac2fb324",
+"canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
---
+++ functions.js
@@ -0,0 +1,165 @@
+
+var client = new dblurt.Client(
+    [
+        "https://rpc.beblurt.com",
+        "https://rpc.blurt.world",
+        "https://blurt-rpc.saboin.com",
+        "https://rpc.blurt.one",
+    ],
+    { timeout: 1500 }
+);
+
+async function runThisJS(jsCode, id) {
+    try {
+        const response = await Promise.race([eval(jsCode), timeOutPromise()]);
+        return replyToNative({
+            data: response,
+            valid: true,
+            error: "",
+        }, id);
+    } catch (e) {
+        return replyToNative({
+            data: null,
+            valid: false,
+            error: e.message,
+        }, id);
+    }
+}
+
+async function loginWithKeychain(id, username) {
+    const challenge = { auth: "login", user: username };
+    const buf = JSON.stringify(challenge, null, 0);
+    return new Promise((resolve) => {
+        try {
+            window.blurt_keychain.requestSignBuffer(
+                username,
+                buf,
+                "Posting",
+                (response) => {
+                    if (response.success === true) {
+                        return resolve(replyToNative({
+                            valid: true,
+                            data: true,
+                            error: null,
+                        }));
+                    } else {
+                        return resolve(createErrorResponse(response));
+                    }
+                }
+            );
+        } catch (error) {
+            return resolve(createErrorResponse(error));
+        }
+    });
+}
+
+async function performOperationsViaKeyChainWeb(
+    operations,
+    username,
+) {
+    let operationList = JSON.parse(decodeURIComponent(escape(atob(operations))));
+    return new Promise((resolve) => {
+        try {
+            const keychain = window.blurt_keychain;
+            keychain.requestBroadcast(
+                username,
+                operationList,
+                "Posting",
+                (response) => {
+                    if (response.success === true) {
+                        return resolve(replyToNative({
+                            valid: true,
+                            data: true,
+                            error: null,
+                        }));
+                    } else {
+                        return resolve(createErrorResponse(response));
+                    }
+                }
+            );
+        } catch (error) {
+            return resolve(createErrorResponse(error));
+        }
+    });
+}
+
+async function pickImageAndUpload(accountName) {
+    return new Promise(function (resolve, reject) {
+        var input = document.createElement("input");
+        input.type = "file";
+        input.onchange = (e) => {
+            var file = e.target.files[0];
+            var reader = new FileReader();
+            reader.readAsBinaryString(file);
+            reader.onload = async () => {
+                const content = window.Buffer.from(reader.result, "binary");
+                const prefix = window.Buffer.from("ImageSigningChallenge");
+                const buf = window.Buffer.concat([prefix, content]);
+                window.blurt_keychain.requestSignBuffer(
+                    accountName,
+                    JSON.stringify(buf),
+                    "Posting",
+                    (response) => {
+                        const url =
+                            `https://img-upload.blurt.world/` +
+                            accountName +
+                            "/" +
+                            response.result;
+                        const formData = new FormData();
+                        formData.append("file", file, file.name);
+                        axios
+                            .post(url, formData)
+                            .then((response) => {
+                                console.log(response.data.url);
+                                return resolve(replyToNative({
+                                    valid: true,
+                                    data: response.data.url,
+                                    error: ""
+                                }));
+                            })
+                            .catch((error) => {
+                                console.log(error.message);
+                                return resolve(replyToNative({
+                                    valid: false,
+                                    data: null,
+                                    error: error.message,
+                                }));
+                            });
+                    }
+                );
+
+            };
+        };
+        input.oncancel = (e) => {
+            return resolve(replyToNative({
+                valid: false,
+                data: null,
+                error: "Image selection cancelled by user"
+            }));
+        }
+        input.click();
+    });
+}
+
+function createErrorResponse(response) {
+    return replyToNative({
+        valid: false,
+        data: null,
+        error: response.message || "Unknown error occurred."
+    });
+}
+
+
+function timeOutPromise(timeout = 15000) {
+    return new Promise((_, reject) => {
+        setTimeout(() => reject(new Error("Server timed out")), timeout);
+    });
+}
+
+function replyToNative(result, id, stringifyResult = true) {
+    if (stringifyResult) {
+        return JSON.stringify(result);
+    } else {
+        return result;
+    }
+}
