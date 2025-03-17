9c54bcc1046abab200bee3746f4959eba27d2e1f
sagar <sag333ar@gmail.com>
2025-03-17 14:25:51 +0530

    updated web app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2670741118"
+    serviceWorkerVersion: "3662995597"
   }
 });
